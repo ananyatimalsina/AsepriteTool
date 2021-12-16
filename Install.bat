75d13316f0826df06286cc1a167ab65733323b2d
TheLiteCrafter <ananyatim333@gmail.com>
2021-12-16 17:50:16 +0100

    Update

--- Install.bat
+++ Install.bat
@@ -1,5 +1,5 @@
 set PATH=%PATH%;%CD%\Git\cmd\
-git config --system http.sslcainfo %CD%\Git\mingw64\ssl\certs\ca-bundle.crt
+git config http.sslCAinfo %CD%\Git\mingw64\ssl\certs\ca-bundle.crt
 cd C:\
 git clone --recursive https://github.com/aseprite/aseprite.git
 mkdir deps
--- Update.bat
+++ Update.bat
@@ -1,5 +1,5 @@
 set PATH=%PATH%;%CD%\Git\cmd\
-git config --system http.sslcainfo %CD%\Git\mingw64\ssl\certs\ca-bundle.crt
+git config http.sslCAinfo %CD%\Git\mingw64\ssl\certs\ca-bundle.crt
 cd C:\aseprite\
 git pull
 git submodule update --init --recursive
