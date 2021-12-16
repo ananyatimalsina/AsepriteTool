91d6ce2b63c69e64416c9fcacf6a59724eaf5619
TheLiteCrafter <ananyatim333@gmail.com>
2021-12-16 15:36:53 +0100

    Updated Install and Update.bat

--- Install.bat
+++ Install.bat
@@ -1,4 +1,5 @@
 set PATH=%PATH%;%CD%\Git\cmd\
+git config --system http.sslverify false
 cd C:\
 git clone --recursive https://github.com/aseprite/aseprite.git
 mkdir deps
--- Update.bat
+++ Update.bat
@@ -1,4 +1,5 @@
 set PATH=%PATH%;%CD%\Git\cmd\
+git config --system http.sslverify false
 cd C:\aseprite\
 git pull
 git submodule update --init --recursive
